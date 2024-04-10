f0ec8c9f7e0715c96188d83337e1ecee2c65015f
Arjun Ranade <epicarjun27@gmail.com>
2024-04-09 18:06:52 -0700

    added backend functionality findUserByName and findUserById

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -35,13 +35,42 @@ const users = {
 
 app.use(express.json());
 
-/*app.get("/", (req, res) => {
+
+
+const findUserByName = (name) => {
+  return users["users_list"].filter(
+    (user) => (user["name"] === name)
+  );
+};
+
+const findUserById = (id) => 
+  users["users_list"].find((user) => user["id"] === id)
+
+
+app.get("/", (req, res) => {
     res.send("Hello World!");
-});*/
+});
 
 app.get("/users", (req, res) => {
+  const name = req.query.name;
+  if (name != undefined) {
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
+  } else {
     res.send(users);
-})
+  }
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"];
+  let result = findUserById(id);
+  if (result == undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
 
 app.listen(port, () => {
     console.log(
