537f9216b7164ae8f4ac4c758bbcd98710c0f542
ArjunR27 <epicarjun27@gmail.com>
2024-04-30 18:32:21 -0700

    exercise 4

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,84 +1,14 @@
 import express from "express"
 import cors from "cors";
 import userServices from "./user-services.js";
-import bodyParser from "body-parser"
 
 
 const app = express();
 const port = 8000
-app.use(bodyParser.json())
 
-const users = {
-    users_list: [
-      {
-        id: "xyz789",
-        name: "Charlie",
-        job: "Janitor"
-      },
-      {
-        id: "abc123",
-        name: "Mac",
-        job: "Bouncer"
-      },
-      {
-        id: "ppp222",
-        name: "Mac",
-        job: "Professor"
-      },
-      {
-        id: "yat999",
-        name: "Dee",
-        job: "Aspring actress"
-      },
-      {
-        id: "zap555",
-        name: "Dennis",
-        job: "Bartender"
-      }
-    ]
-  };
 
 app.use(cors());
 app.use(express.json());
-
-
-
-/*const findUserByName = (name) => {
-  return users["users_list"].filter(
-    (user) => (user["name"] === name)
-  );
-};
-
-const findUserByNameAndJob = (name, job) => {
-  return users["users_list"].filter(
-    (user) => (user["name"] === name && user["job"] === job)
-  );
-};
-
-const findUserById = (id) => 
-  users["users_list"].find((user) => user["id"] === id)
-
-const addUser = (user) => {
-  users["users_list"].push(user);
-  return user;
-};
-
-const deleteUser = (id) => {
-  const indexToDelete = users["users_list"].findIndex(user => user.id === id);
-  console.log(indexToDelete)
-  if (indexToDelete != -1) {
-    users["users_list"].splice(indexToDelete, 1)
-    return true;
-  } else {
-    return false; 
-  }
-};
-
-const generateID = () => {
-  const id = (Math.random() * 1000).toFixed(0)
-  return id
-} */
-
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
@@ -111,7 +41,7 @@ app.post("/users", async (req, res) => {
   const user = req.body;
   const savedUser = await userServices.addUser(user);
   if (savedUser) res.status(201).send(savedUser);
-  else res.status(500).end();
+  else res.status(500).send();
 });
 
 app.delete("/users/:id", async (req, res) => {
